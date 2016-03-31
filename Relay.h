72cf8cf45e479d0e8e297dffd5026dcf7e81d659
rafaelnsantos <rafaelnsantos@users.noreply.github.com>
2016-03-30 21:39:59 -0300

    Update Relay.h

--- Relay.h
+++ Relay.h
@@ -3,12 +3,8 @@
 #ifndef _RELAY_h
 #define _RELAY_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-  #include <Arduino.h>
-#else
-	#include "WProgram.h"
-#endif
-
+#include <Arduino.h>
+  
 class Relay
 {
  protected:
